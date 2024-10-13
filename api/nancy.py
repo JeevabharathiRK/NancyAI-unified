62d2f98ed6c5182ed434736716acd939704d7c76
JEEVA BHARATHI <JeevabharathiRK@gmail.com>
2024-10-13 18:05:59 +0530

    Update nancy.py

--- api/nancy.py
+++ api/nancy.py
@@ -3,11 +3,12 @@ from meta_ai_api import MetaAI
 class Nancy:
     def __init__(self):
         self.name = "Nancy"
-        self.proxy = { 
-        'http': '47.178.24.220:80',
-        'https': '198.24.187.93:8001'
-        }
-        self.meta = MetaAI(proxy=self.proxy)
+        # self.proxy = { 
+        # 'http': '47.178.24.220:80',
+        # 'https': '198.24.187.93:8001'
+        # }
+        # self.meta = MetaAI(proxy=self.proxy)
+        self.meta = MetaAI()
         self.meta.prompt("remember your name is Nancy")
 
     def prompt(self, msg):
